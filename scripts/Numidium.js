c7db208f6c28872345e070b44b1519434e330e99
Thomas Grelecki <tgrelecki@gmail.com>
2013-10-13 13:41:06 -0500

    Refactored Oculus/Anaglyph/Normal Toggle
    Added the ability to toggle the different viewing modes by using 1,2, &
    3.

--- scripts/Numidium.js
+++ scripts/Numidium.js
@@ -1,28 +1,24 @@
-// MAIN
-// need a new strategy for loading in IE... require.js?
-// enable webgl in firefox; use latest nightly build for very slight perf. increase...
-// standard global variables
-var container, scene, camera, renderer, controls, stats, oculuscontrol, effect, realcamera;
-var clock = new THREE.Clock();
 
-// custom global variables
+var container, scene, camera, renderer, controls, stats, oculuscontrol, oculusEffect, anaglyphEffect, realcamera;
+var isOculus, isAnaglyph;
+var clock = new THREE.Clock();
 var lighthouse;
 
 init();
 animate();
 
-// FUNCTIONS
 function init() 
 {
 	container = document.body;
 
 	// SCENE
 	scene = new THREE.Scene();
+	
 	// CAMERA
 	var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+	var VIEW_ANGLE = 60, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
 	
-		// RENDERER
+	// RENDERER
 	if ( Detector.webgl ) {
 		renderer = new THREE.WebGLRenderer( {antialias:false} );
 	}
@@ -30,56 +26,32 @@ function init()
 		renderer = new THREE.CanvasRenderer();
 	}	
 	
-	if(true) { //oculus rift
-		
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		
-		camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 5000 );
-		//camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-		camera.position.set(0,150,400);
-		//camera.lookAt(scene.position);	
-		
-		effect = new THREE.OculusRiftEffect( renderer, { worldScale: 1 } );
-		effect.setSize( window.innerWidth, window.innerHeight );
-		
-		effect.separation = 20;
-		effect.distortion = 0.1;
-		effect.fov = 110;
-		
-		container.appendChild( renderer.domElement );
-		
-		controls = new THREE.FirstPersonControls( camera );
-		controls.movementSpeed = 400;
-		controls.lookSpeed = 0.15;
-		controls.lookVertical = true;
+	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+	camera.position.set(0,150,400);
+	camera.lookAt(scene.position);		
+	scene.add(camera);
+			
+	// CONTROLS
+	controls = new THREE.FirstPersonControls( camera );
+	controls.movementSpeed = 400;
+	controls.lookSpeed = 0.15;
+	controls.lookVertical = true;
+	
+	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	container = document.body;
+	container.appendChild( renderer.domElement );
 
-		oculuscontrol = new THREE.OculusControls( camera );
-		
-		//document.body.appendChild( renderer.domElement );
-		
-				
-	}
-	else {
-		camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-		camera.position.set(0,150,400);
-		camera.lookAt(scene.position);		
-		scene.add(camera);
-		// CONTROLS
-		controls = new THREE.OrbitControls( camera, renderer.domElement );
-		
+	// EFFECTS
+	isOculus = false;
+	oculusEffect = new THREE.OculusRiftEffect( renderer, {worldFactor: 1} );
+	oculusEffect.setSize( window.innerWidth, window.innerHeight );
+	oculuscontrol = new THREE.OculusControls( camera );
+	oculuscontrol.connect();
 	
-		oculuscontrol = new THREE.OculusControls( camera );
-	
-		renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-		container = document.body;
-		container.appendChild( renderer.domElement );
-	}
-	
+	isAnaglyph = false;
+	anaglyphEffect = new THREE.AnaglyphEffect(  renderer, SCREEN_WIDTH, SCREEN_HEIGHT );
+	anaglyphEffect.setSize( window.innerWidth, window.innerHeight );
 
-
-	// EVENTS
-	//window resize, fullscreen, oculus enable, audio mute, IPD adjust
-	
 	// STATS
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
@@ -97,11 +69,12 @@ function init()
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
 	floorTexture.repeat.set( 10, 10 );
 	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+	var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 10, 10);
 	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	floor.position.y = -80;
+	floor.position.y = -90;
 	floor.rotation.x = Math.PI / 2;
 	scene.add(floor);
+	
 	// SKYBOX/FOG
 	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
 	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
@@ -111,50 +84,71 @@ function init()
 	
 	//var jsonLoader = new THREE.JSONLoader();
 	//jsonLoader.load( "models/lighthouse/lighthouse.js", addModelToScene );
-	var manager = new THREE.LoadingManager();
-				manager.onProgress = function ( item, loaded, total ) {
-
-					console.log( item, loaded, total );
-
-				};
 	
-	//var loader = new THREE.OBJMTLLoader();
-	//			loader.load( 'models/Old Building 28/Old Building 28-OBJ/Old Building 28.obj', 'models/Old Building 28/Old Building 28-OBJ/Old Building 28.mtl', function ( object ) {
-	//				//object.position.y = - 80;			object.scale = new THREE.Vector3( 10, 10, 10 );
-	//				scene.add( object );
-	//			} );
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+
+		console.log( item, loaded, total );
+
+	};
+	
+	// var loader = new THREE.OBJMTLLoader();
+	// loader.load( 'models/Old Building 28/Old Building 28-OBJ/Old Building 28.obj', 'models/Old Building 28/Old Building 28-OBJ/Old Building 28.mtl', function ( object ) {
+		// object.position.y = - 80;			
+		// object.scale = new THREE.Vector3( 10, 10, 10 );
+		// scene.add( object );
+	// } );
 				
 	var loader = new THREE.OBJLoader( manager );
 	loader.load( 'models/Old Building 28/Old Building 28-OBJ/Old Building 28.obj', function ( object ) {
-		object.scale = new THREE.Vector3( 10, 10, 10 );
-		object.position.y = - 80;
+		object.scale = new THREE.Vector3( 50, 50, 50 );
+		object.position.y = - 90;
 		scene.add( object );
 	});
-
-
 	
 	var ambientLight = new THREE.AmbientLight(0xFF1111);
 	scene.add(ambientLight);
 	
 	window.addEventListener( 'resize', onWindowResize, false );
 	document.addEventListener('keydown', keyPressed, false);
-
-	oculuscontrol.connect();
 }
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	realcamera.aspect = window.innerWidth / window.innerHeight;
-	realcamera.updateProjectionMatrix();
-	effect.setSize( window.innerWidth, window.innerHeight );
+	
 	controls.handleResize();
+
+	oculusEffect.setSize( window.innerWidth, window.innerHeight );
+	anaglyphEffect.setSize( window.innerWidth, window.innerHeight );
 }
 
 function keyPressed(event) {
-	if (event.keyCode === 72) { // H
+	var key = event.keyCode;
+	
+	if (key === 72) { // H
 		guiVisible = !guiVisible;
 		document.getElementById('info').style.display = guiVisible ? "block" : "none";
+	}
+	
+	switch(key) {
+		case 49 :
+			isOculus = false;
+			isAnaglyph = false;
+			onWindowResize();
+			break;
+		case 50 :
+			isOculus = true;
+			isAnaglyph = false;
+			//onWindowResize(); //Doesn't seem to be necessary
+			break;
+		case 51 :
+			isOculus = false;
+			isAnaglyph = true;
+			onWindowResize();
+			break;
+		default:
+			break;
 	}
 }
 
@@ -169,20 +163,20 @@ function addModelToScene( geometry, materials )
 function animate() 
 {
 	requestAnimationFrame( animate );
-
 	render();		
 	update();
 }
 
 function update()
 {
-	//listen for keyboard/HMD/mouse controls	
-	controls.update(clock.getDelta() );
-	oculuscontrol.update(clock.getDelta() );
 	stats.update();
+	controls.update(clock.getDelta() );
+	if(isOculus) oculuscontrol.update(clock.getDelta());
 }
 
 function render() 
-{
-	renderer.render( scene, camera );
+{ 
+	if(isOculus) oculusEffect.render( scene, camera );
+	else if(isAnaglyph) anaglyphEffect.render( scene, camera );
+	else renderer.render( scene, camera );
 }
