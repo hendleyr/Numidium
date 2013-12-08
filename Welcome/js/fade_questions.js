e5638d0e147015c3abbe304a15966d08cedb9068
Tommy Grelecki <tgrelecki@gmail.com>
2013-12-08 17:10:25 -0600

    Updated welcome page
    Updated oculus-rest how to

---
+++ Welcome/js/fade_questions.js
@@ -0,0 +1,23 @@
+$("#q1").on('click', function() {
+	$("#a1").fadeToggle();
+});
+
+$("#q2").on('click', function() {
+	$("#a2").fadeToggle();
+});
+
+$("#q3").on('click', function() {
+	$("#a3").fadeToggle();
+});
+
+$("#q4").on('click', function() {
+	$("#a4").fadeToggle();
+});
+
+$("#q5").on('click', function() {
+	$("#a5").fadeToggle();
+});
+
+$("#q6").on('click', function() {
+	$("#a6").fadeToggle();
+});
